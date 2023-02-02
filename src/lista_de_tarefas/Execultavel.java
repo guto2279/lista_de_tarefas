c31d581f6da9629933dfcce567534c1f4d815145
wesle <wesle@Prog-PC>
2023-02-02 13:12:31 -0300

    Criado um loop do while, para pegar a informação do usuário diversas vezes, criado uma lista de tarefas, e adicionado um método de adicionar mostrar tarefas

--- src/lista_de_tarefas/Execultavel.java
+++ src/lista_de_tarefas/Execultavel.java
@@ -7,30 +7,36 @@ import java.util.Scanner;
 public class Execultavel {
 	public static void main(String [] args) {
 			
+			// Declaração de variáveis
 			int opTarefa;
 			String adTarefa;
 			
-			Tarefas tarefas = new Tarefas();
-			List <Tarefas> listaTarefas = new ArrayList<Tarefas>();		
+			// Instanciando Classe Tarefas, Scanner 
+			Tarefas tarefas = new Tarefas();	
 			Scanner scanner = new Scanner(System.in);
 			
-			System.out.println("---------------- Lista de Tarefas ----------------");
-			System.out.println("Selecione a opção abaixo: ");
-			System.out.println("1. Adicionar tarefa ");
-			System.out.println("2. Listar tarefas ");
-			System.out.println("3. Modificar tarefas");
-			System.out.println("4. Excluir tarefas");
+			do {
+				// Apresentando opções aos usuários 
+				System.out.println("---------------- Lista de Tarefas ----------------");
+				System.out.println("Selecione a opção abaixo: ");
+				System.out.println("0. Excluir tarefas");
+				System.out.println("1. Adicionar tarefa ");
+				System.out.println("2. Listar tarefas ");
+				System.out.println("3. Modificar tarefas");
+				System.out.println("4. Excluir tarefas");
+				
+				// Pegando opções escolhidas pelo usuario 
+				opTarefa = scanner.nextInt();
+				
+				
+				if (opTarefa == 1) {
+					tarefas.adicionarTarefas();
+					tarefas.listarTarefas();
+				}else if (opTarefa == 2) {
+					tarefas.listarTarefas();
+				}
 			
-			opTarefa = scanner.nextInt();
-			
-			if (opTarefa == 1) {
-				tarefas.adicionarTarefas();
-				listaTarefas.add(tarefas);
-			}
-			
-			for (Tarefas tarefas2 : listaTarefas) {
-				System.out.println(tarefas2.getTarefa());
-			}
+			}while(opTarefa != 0);
 			
 		}
 	}
--- src/lista_de_tarefas/Tarefas.java
+++ src/lista_de_tarefas/Tarefas.java
@@ -1,16 +1,26 @@
 package lista_de_tarefas;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tarefas {
 	
+	Execultavel execultavel = new Execultavel();
+	private List <String> listaTarefas = new ArrayList<String>();
 	
 	String tarefa;
 	Scanner scanner = new Scanner (System.in);
 	
-	public String adicionarTarefas(){
+	public void adicionarTarefas(){
 		System.out.println("Qual tarefa deseja adicionar? ");
 		tarefa = scanner.nextLine();
-		return tarefa;
+		listaTarefas.add(tarefa);
+	}
+	
+	public void listarTarefas(){
+		for (String tarefas : this.listaTarefas) {
+			System.out.println(tarefas);
+		}
 	}
 
 	public String getTarefa() {
@@ -20,6 +30,16 @@ public class Tarefas {
 	public void setTarefa(String tarefa) {
 		this.tarefa = tarefa;
 	}
+
+	public List<String> getListaTarefas() {
+		return listaTarefas;
+	}
+
+	public void setListaTarefas(List<String> listaTarefas) {
+		this.listaTarefas = listaTarefas;
+	}
+	
+	
 	
 	
 	
